b470be09347b52a4f0e269c3a5699e085a57c193
wilmerx5 <wilmercampos2004@gmail.com>
2024-08-16 11:27:52 -0500

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import AuthApi from '@/services/AuthApi'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('@/views/auth/LogInView.vue'),
     },
     {
       path: '/admin',
